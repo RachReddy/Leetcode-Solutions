aa997c564f65e2532d8feb2ceb0e5bca29ce7736
Rachana NV <94166047+RachReddy@users.noreply.github.com>
2024-02-18 20:04:38 +0530

    Update Maximum_Subarray.java

--- Arrays/Medium/Maximum_Subarray.java
+++ Arrays/Medium/Maximum_Subarray.java
@@ -1,9 +1,9 @@
 
 public class Maximum_Subarray {
-	
+
+	/*
 	 public static int maxSubArray(int[] nums)
-	 {
-	        
+	 {	        
 	        int max_value=Integer.MIN_VALUE;
 	        for(int s=0;s<nums.length;s++)
 	        {
@@ -19,7 +19,28 @@ public class Maximum_Subarray {
 	        }
 	        return max_value;
 	  }
+	*/
 
+	public static int maxSubArray(int[] nums)   //Kadane's Algorithm
+	{	
+            
+            int max_value=Integer.MIN_VALUE;
+	    int sum=0;
+	    for(int s=0;s<nums.length;s++)
+	    {
+	        sum=sum+nums[s];   //keep adding elements to sum	        
+                max_value=Math.max(max_value,sum); 
+
+	        if(sum<0)             //if sum<0 or -ve, then drop it (means, re-initialize sum to 0)
+                {                     //else sum>0, do nothing, keep it     
+	          sum=0;
+	        }
+             }
+	    return max_value;
+	 }
+	
+
+	
 	public static void main(String[] args)
 	{
 		int[] nums={5,4,-1,7,8};
