639cdb20465d060b0f97419d9e8e91065bf7489f
Rachana NV <94166047+RachReddy@users.noreply.github.com>
2024-02-20 23:24:54 +0530

    Add files via upload

---
+++ Stacks/Nearest_smallest_on_left.java
@@ -0,0 +1,34 @@
+package Stacks;
+
+public class Nearest_smallest_on_left {
+
+	public static void main(String[] args) {
+
+		int[] nums= {4,3,1,2,7,8};
+		int n=nums.length;
+		int[] res=new int[n];
+		
+		//B.F  O(n2)
+		res[0]=0;
+		for(int i=1;i<n;i++)
+		{
+			for(int j=i-1;j>=0;j--)
+			{
+				if(nums[j]<nums[i])
+				{
+					res[i]=nums[j];
+					break;
+				}
+			}
+		}
+		
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(res[i]+", ");
+		}
+		
+
+
+	}
+
+}
