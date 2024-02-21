a0c26e2995633414eae93aa168fbcd6ad6763bd6
Rachana NV <94166047+RachReddy@users.noreply.github.com>
2024-02-21 13:03:22 +0530

    Update Nearest_greatest_on_left.java

--- Stacks/Nearest_Greater/Nearest_greatest_on_left.java
+++ Stacks/Nearest_Greater/Nearest_greatest_on_left.java
@@ -1,9 +1,11 @@
 package Stacks;
+import java.util.Stack;
 
 public class Nearest_greatest_on_left {
 
 	public static void main(String[] args) {
 
+	/*	
 		int[] nums= {4,3,1,2,7,8};
 		//	     0,4,3,3,0,0
 		int n=nums.length;
@@ -27,7 +29,44 @@ public class Nearest_greatest_on_left {
 		{
 			System.out.print(res[i]+", ");
 		}
+     */
 
+      //OPTIMIZED: t.c O(2N) => O(N)   s.c= O(N)
+                int[] nums= {4,3,1,2,7,8};
+		//	     0,4,3,3,0,0
+		int n=nums.length;
+		int[] res=new int[n];		
+		Stack stack=new Stack();
+		
+		//res[0]=0; this line not required
+		for(int i=0;i<n;i++)
+		{
+			int cur_elem= nums[i];
+			
+			//STEP1                             top <= cur elem
+			while( !stack.isEmpty() && (int)stack.peek() <= cur_elem)
+			{
+				stack.pop();
+			}
+			
+			//STEP2
+			if(stack.isEmpty()) 
+				res[i]=0;
+			else
+				res[i]= (int)stack.peek();      //res[i] = top
+			
+			//STEP3
+			stack.push(cur_elem);
+			
+		}
+
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(res[i]+", ");
+		}		
+		
 	}
-
 }
+//T.C= O(N) = here we are pushing each elem in stack & poping each elem in stack only once.
+//  	    - we have 2 iterations for each elem
+//	    - O(2N) => O(N)	
